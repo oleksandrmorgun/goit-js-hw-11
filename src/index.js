55277796dbd2bedb628891002557c3a83e759107
oleksandrmorgun <127879303+oleksandrmorgun@users.noreply.github.com>
2023-08-20 23:31:42 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,83 @@
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const btnLoadMoreEl = document.querySelector('.load-more');
+
+const pixabayAPI = new PixabayAPI();
+
+let gallery = new SimpleLightbox('.gallery a');
+
+formEl.addEventListener('submit', handleFormSubmit);
+btnLoadMoreEl.addEventListener('click', handleLoadMoreBtnClick);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  galleryEl.innerHTML = '';
+  btnLoadMoreEl.classList.add('is-hidden');
+  const form = event.currentTarget;
+  const searchQuery = form.elements['searchQuery'].value.trim();
+  pixabayAPI.q = searchQuery;
+
+  if (!searchQuery) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+
+  searchGallery();
+}
+
+async function searchGallery() {
+  try {
+    const { data } = await pixabayAPI.fetchImgs();
+
+    if (data.totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    galleryEl.innerHTML = createGallery(data.hits);
+
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+    gallery.refresh();
+
+    if (data.totalHits > pixabayAPI.per_page) {
+      btnLoadMoreEl.classList.remove('is-hidden');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function handleLoadMoreBtnClick() {
+  pixabayAPI.page += 1;
+  searchLoadMoreImg();
+}
+
+async function searchLoadMoreImg() {
+  try {
+    const { data } = await pixabayAPI.fetchImgs();
+
+    galleryEl.insertAdjacentHTML('beforeend', createGallery(data.hits));
+    gallery.refresh();
+
+    // const { height: cardHeight } =
+    //   galleryEl.firstElementChild.getBoundingClientRect();
+
+    // window.scrollBy({
+    //   top: cardHeight * 2,
+    //   behavior: 'smooth',
+    // });
+
+    if (data.hits.length < pixabayAPI.per_page) {
+      btnLoadMoreEl.classList.add('is-hidden');
+      return Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
